9e9a657ad4f950f4cd14a9700c8f83484d59b4e9
daniboyrc <danielrcifpb@gmail.com>
2017-11-01 15:49:16 -0300

    Adicionados comentarios a classe Exceptions

--- coisa/src/coisa/Exceptions.java
+++ coisa/src/coisa/Exceptions.java
@@ -1,15 +1,26 @@
 package coisa;
 
+/**
+ * Funcoes que imprimem mensagem em caso de erro.
+ * 
+ * @author Daniel Coura
+ */
 public class Exceptions {
+	
+	/**
+	 * Imprime mensagem de erro no caso do comando receber mais ou menos 
+	 * argumentos do que o necessário. 
+	 * 
+	 * @param comando o comando com erro na quantidade de argumentos
+	 * @param numeroArgumentos o numero de argumentos passados
+	 * @param minimo o numero minimo de argumentos que o comando exige
+	 * @param maximo o numero maximo de argumentos que o comando exige
+	 */
 	public static void numeroArgumentos(String comando, int numeroArgumentos, int minimo, int maximo) {
 		if (numeroArgumentos > maximo) { 
 			System.out.printf("O comando %s suporta até %d argumentos", comando, maximo);
 		} else if (numeroArgumentos < minimo) {
 			System.out.printf("O comando %s recebe pelo menos %d argumentos\n", comando, minimo);
 		}
-	}
-	
-	public static void nomeRepetido(String palavra, String contexto) {
-		System.out.printf("Já existe %s em %s", palavra, contexto);
 	}
 }
